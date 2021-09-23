597a8d400c721f13b75df8dbad911615007537b6
Sylvester <nnaemekasilver@gmail.com>
2021-09-23 16:54:13 +0100

    Add add, clear completed and edit functionality

--- src/status-update.js
+++ src/status-update.js
@@ -1,7 +1,34 @@
-import { list, save } from './storage';
+// eslint-disable-next-line import/no-cycle
+import { displayTasks, items } from './add-remove';
+// eslint-disable-next-line import/no-mutable-exports
+export let list = [];
+
+if (localStorage.getItem('list')) {
+  list = JSON.parse(localStorage.getItem('list'));
+}
+
+export const save = () => {
+  localStorage.setItem('list', JSON.stringify(list));
+};
+
+export function fixIndex(list) {
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].index = i;
+  }
+}
+
+export function setList(filter) {
+  list = [];
+  for (let i = 0; i < filter.length; i += 1) {
+    list[i] = filter[i];
+    save();
+    displayTasks();
+  }
+}
 
 export default function checkboxesEvent() {
   const temp = list;
+
   const checkboxes = document.getElementsByClassName('checkbox');
   for (let i = 0; i < checkboxes.length; i += 1) {
     checkboxes[i].addEventListener('change', () => {
@@ -12,7 +39,24 @@ export default function checkboxesEvent() {
         temp[i].completed = true;
         document.getElementById(`desc-${temp[i].index}`).classList.add('completed');
       }
-      save(temp);
+      setList(temp);
+    });
+  }
+}
+
+export function remove() {
+  const temp = list;
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].children[3].addEventListener('click', (event) => {
+      if (event.target) {
+        temp.splice(i, 1);
+        fixIndex(temp);
+        setList(temp);
+        save();
+      }
+      checkboxesEvent();
+      displayTasks();
+      remove();
     });
   }
 }
