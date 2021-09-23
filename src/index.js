f15d81d69c0e1f0b96851328effe6516b1b312e9
Sylvester <nnaemekasilver@gmail.com>
2021-09-23 13:21:03 +0100

    Add list properties

--- src/index.js
+++ src/index.js
@@ -1,14 +1,64 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todoList = document.getElementById('todo-list');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const list = [
+  {
+    description: 'Start project',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Install Webpack',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Add list structure',
+    completed: false,
+    index: 2,
+  },
+];
 
-  return element;
-}
+const createTask = (task) => {
+  let todoObj = '';
+  if (task.completed === true) {
+    todoObj = `
+        <article id="${task.index}" class="task-item" draggable="true">
+          <input type='checkbox' name='completed' class="checkbox" checked>
+          <span class='task-description completed' id="desc-${task.index}" contenteditable>${task.description}</span>
+          <i class="bi bi-three-dots-vertical"></i>
+          <i class="bi bi-trash hide"></i>
+        </article>`;
+  } else {
+    todoObj = `
+          <article  id="${task.index}" class="task-item" draggable="true">
+            <input type='checkbox' name='completed' class="checkbox">
+            <span class='task-description' id="desc-${task.index}" contenteditable>${task.description}</span>
+            <i class="bi bi-three-dots-vertical"></i>
+            <i class="bi bi-trash hide"></i>
+          </article>`;
+  }
 
-document.body.appendChild(component());
+  todoList.innerHTML += todoObj;
+};
+
+const displayTasks = (list) => {
+  todoList.innerHTML = '';
+
+  const sortedList = list.sort((a, b) => {
+    if (a.index > b.index) {
+      return 1;
+    }
+    if (a.index < b.index) {
+      return -1;
+    }
+    return 0;
+  });
+
+  sortedList.forEach((task) => {
+    createTask(task);
+  });
+};
+
+displayTasks(list);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  watch: true,
   devServer: {
     static: './dist',
   },
